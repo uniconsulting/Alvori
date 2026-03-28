9cef50291ce013ccb194ffe73547d5be1bbe5904
uni.kit <uni.ai.consalting@gmail.com>
2026-03-28 08:29:09 +0400

    Update fonts.ts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,13 +3,18 @@ import localFont from 'next/font/local';
 const garet = localFont({
   src: [
     {
-      path: '../fonts/garet/Garet-Book.woff2',
+      path: '../fonts/garet/Garet-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../fonts/garet/Garet-Heavy.woff2',
-      weight: '800',
+      path: '../fonts/garet/Garet-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/garet/Garet-Bold.woff2',
+      weight: '700',
       style: 'normal',
     },
   ],
