65789657e475afadf525f5a9bb71eb09678a30a7
uni.kit <uni.ai.consalting@gmail.com>
2026-04-01 23:09:07 +0400

    Update HeroLeftScene.tsx

--- src/components/sections/hero/HeroLeftScene.tsx
+++ src/components/sections/hero/HeroLeftScene.tsx
@@ -359,7 +359,7 @@ export function HeroLeftScene() {
         </div>
 
         <div className="pointer-events-none absolute inset-0 xl:hidden">
-          <div className="absolute left-[162px] top-[40px] w-[240px]">
+          <div className="absolute left-[162px] top-[40px] w-[234px]">
             <div
               key={activeSlide}
               className={cn(
